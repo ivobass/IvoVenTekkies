3228b645ffbf9198a026541c1367d2a0e50b113e
Ivo Baptista <ivobass@gmail.com>
2020-03-26 11:52:32 +0000

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ivo/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ivo/Desktop/IvoApps/ivoventek"
+export "FLUTTER_TARGET=/Users/ivo/Desktop/IvoApps/ivoventek/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/ivo/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
